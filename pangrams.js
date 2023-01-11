9e98d9b7652e78b52706c1bca8a5e3038cfa5453
Doug-Sales <douglasmsales28@outlook.com>
2023-01-10 22:41:12 -0300

    Pangram

---
+++ pangrams.js
@@ -0,0 +1,14 @@
+function pangrams(s) {
+    const alphabet = new Set('abcdefghijklmnopqrstuvwxyz');
+    const setPangram = new Set(s.toLowerCase().replace(/[^a-z]/g, ''));
+
+    for (const letter of alphabet) {
+        if (!setPangram.has(letter)) {
+            return 'not pangram';
+        }
+
+    }
+    return 'pangram'
+}
+//https://www.hackerrank.com/challenges/pangrams
+// Pangrams
