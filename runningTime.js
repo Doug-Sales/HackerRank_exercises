b2803ac210ade9db5f985cfcbfe78aa116655ce0
Doug-Sales <douglasmsales28@outlook.com>
2023-01-12 21:36:59 -0300

    Running Time of Algorithms

---
+++ runningTime.js
@@ -0,0 +1,16 @@
+function runningTime(arr) {
+    let swapCounter = 0;
+
+    for (let i = 0; i < arr.length - 1; i++) {
+        for (let j = 0; j < arr.length - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                let temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+                swapCounter++
+            }
+        }
+    }
+    return swapCounter
+}
+//https://www.hackerrank.com/challenges/runningtime/
