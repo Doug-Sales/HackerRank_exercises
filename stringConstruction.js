d7958d37b57b64630a12d1226c01bd8a6921f050
Doug-Sales <douglasmsales28@outlook.com>
2023-01-15 19:39:14 -0300

    String Construction

---
+++ stringConstruction.js
@@ -0,0 +1,7 @@
+function stringConstruction(s) {
+    const appendCost = new Set(s).size
+
+    return appendCost;
+}
+//https://www.hackerrank.com/challenges/string-construction
+// Big-O: O(n) - temporal e espacial.
